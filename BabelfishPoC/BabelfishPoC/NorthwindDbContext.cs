050bbafe3b7ad3e97670f71391608febf59b2dd4
workshop-1c71e191-b6a2-4a53-8cf0-4fea15eb651b <user@codecommit.aws>
2022-01-14 13:47:31 -0800

    Initial Commit

---
+++ BabelfishPoC/BabelfishPoC/NorthwindDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BabelfishPoC
+{
+    public class NorthwindDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\\mssqllocaldb;Database=NorthwindTraders;Trusted_Connection=True;MultipleActiveResultSets=true;Application Name=NorthwindTraders;");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(NorthwindDbContext).Assembly);
+        }
+    }
+}
---
+++ BabelfishPoC/BabelfishPoC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BabelfishPoC
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello Babelfish!");
+
+            using (var dbContext = new NorthwindDbContext())
+            {
+                var seeder = new SampleDataSeeder(dbContext);
+
+                await seeder.SeedCustomersAsync();
+
+                var customer = await dbContext.Customers.FirstOrDefaultAsync(c => c.CustomerId == "GALED");
+
+                Console.WriteLine(customer.Address);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    
+
+}
